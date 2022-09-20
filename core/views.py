92c7fec87bcb04938d33a991e3dbce22c3e57ba7
Uadson Emile Castro Feitosa <uadsonpy@gmail.com>
2022-09-20 15:27:12 -0300

    created view

--- core/views.py
+++ core/views.py
@@ -4,11 +4,26 @@ from googletrans import Translator
 
 def index(request):
 	translator = Translator()
-	
-	text = translator.translate('Olá Mundo', dest='en')
 
-	context = {
-		'text': text.text 
-	}
+	if not request.POST:
+		return render(request, 'core/index.html')
 
-	return render(request, 'core/index.html', context)
+	if request.POST:
+		input_text = request.POST.get('inputText')
+		
+		output_lang = request.POST.getlist('lang[]')
+		
+		try:
+			if len(output_lang) == 1:
+				for lang in output_lang:
+					data = translator.translate(input_text, dest=lang)
+
+					print(data.text)
+
+					context = {
+						'data': data.text,
+					}
+		except:
+
+
+		return render(request, 'core/index.html', context)
