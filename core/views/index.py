e40cb5b125c1a0c29d74787fe977010b2ea05020
Uadson Emile Castro Feitosa <uadsonpy@gmail.com>
2022-09-23 14:15:53 -0300

    adds lib for convert text to audio

--- core/views/index.py
+++ core/views/index.py
@@ -1,9 +1,23 @@
 from django.shortcuts import render, redirect
 from googletrans import Translator
 from django.contrib import messages
+from gtts import gTTS
+import os
+from pathlib import Path
+
+
+
+BASE = Path(__file__).resolve().parent.parent.parent
 
 
 def index(request):
+	"""
+		Translator
+
+		#1 received text typed by user and translate it
+		#2 the text is converted in a audio file
+	"""
+
 	if not request.POST:
 		return render(request, 'core/index.html')
 
@@ -20,12 +34,15 @@ def index(request):
 	
 	if len(output_lang) == 1:
 		for lang in output_lang:
+			# translating
 			data = translator.translate(input_text, dest=lang)
-			
-			request.session['data'] = data.text
+			# converting and saving the converted audio
+			audio = gTTS(text=data.text, lang=lang, slow=False)
+			audio.save("./media/audio.mp3")
 			# creating session
-			response = request.session.get('data')
+			request.session['data'] = data.text
 			# accessing session
+			response = request.session.get('data')
 			
 			context = {'response': response}
 
