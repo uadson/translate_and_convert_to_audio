ae6f2d242bcd8a2cc2e0e2becfe73bb446297a11
Uadson Emile Castro Feitosa <uadsonpy@gmail.com>
2022-09-28 13:27:46 -0300

    project path changed

--- core/urls/index.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from core.views.index import index
-
-
-urlpatterns = [
-	path('', index, name='index'),
-]
--- utils/digger.py
+++ utils/digger.py
@@ -2,8 +2,29 @@ import os
 from pathlib import Path
 
 
-BASE = Path(__file__).resolve().parent.parent.parent
+BASE = Path(__file__).resolve().parent.parent
 
 
-def get_media():
-	pass
+def search():
+	path_dir = os.listdir(BASE)
+	for i in range(len(path_dir)):
+		if path_dir[i] == 'media':
+			return i
+
+
+def get_media_folder():
+	media_dir = search()
+	path_dir = os.listdir(BASE)
+	return path_dir[media_dir]
+
+
+def delete_file(file):
+	file = f"{file}.mp3"
+	media_folder = get_media_folder()
+	media_path = os.path.join(BASE, media_folder)
+	media = os.path.join(media_path, file)
+	os.remove(media)
+
+
+if __name__ == '__main__':
+	delete()
